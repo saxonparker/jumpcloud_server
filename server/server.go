b097a766e8abd2cbe1a08ce3017a7a5df694abc7
Saxon Parker <saxon@bestateless.com>
2020-02-14 09:17:58 -0700

    added stats endpoint

--- server/server.go
+++ server/server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"crypto/sha512"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -30,7 +31,11 @@ type HashServer struct {
 	hashDelay time.Duration
 
 	// Tracks the number of requests that have been made thus far
-	count    uint64
+	count uint64
+	// Tracks the average time of POST requests
+	avgUsec int64
+	// Both count and avgUsec are protected by this lock. Each value could have its own lock, but this makes it
+	// easier for the stats endpoint to grab one lock and get both fields.
 	countMux sync.Mutex
 
 	// Channel to send passwords to worker to be hashed
@@ -43,9 +48,6 @@ type HashServer struct {
 	// Shutdown bool to indicate if the server has been shutdown
 	shutdown    bool
 	shutdownMux sync.Mutex
-
-	// Stats
-
 }
 
 func NewHashServer(port uint, hashDelay time.Duration, chanSize uint64) *HashServer {
@@ -53,6 +55,7 @@ func NewHashServer(port uint, hashDelay time.Duration, chanSize uint64) *HashSer
 	p.port = port
 	p.hashDelay = hashDelay
 	p.count = 0
+	p.avgUsec = 0
 	p.hashRequests = make(chan *passwordRequest, chanSize)
 	p.hashedPasswords = make(map[uint64]string)
 	p.shutdown = false
@@ -108,6 +111,8 @@ func (hs *HashServer) hashGetHandler(w http.ResponseWriter, r *http.Request) {
 func (hs *HashServer) hashPostHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
+		timeStart := time.Now()
+
 		// Parse the request
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "Could not parse request. Error: %v\n", err)
@@ -126,11 +131,50 @@ func (hs *HashServer) hashPostHandler(w http.ResponseWriter, r *http.Request) {
 		hs.countMux.Unlock()
 
 		// Pass the new request to worker to actually perform the request
-		req := newPasswordRequest(password, id, time.Second*5)
+		req := newPasswordRequest(password, id, hs.hashDelay)
 		hs.hashRequests <- req
 		fmt.Fprintf(w, "%d", id)
 
+		// Calculate the elapsed time and update the average
+		timeEnd := time.Now()
+		elapsed := int64(timeEnd.Sub(timeStart) / time.Microsecond)
+		hs.countMux.Lock()
+		// Moving average calculation taken from https://stackoverflow.com/a/37830174
+		hs.avgUsec = hs.avgUsec + (elapsed-hs.avgUsec)/int64(id)
+		hs.countMux.Unlock()
+
 	// Only POST requests allowed
+	default:
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+	}
+}
+
+// Helper struct for marshalling the stats JSON
+type stats struct {
+	Total   uint64 `json:"total"`
+	Average int64  `json:"average"`
+}
+
+// Handles requests to the /stats endpoint
+func (hs *HashServer) statsHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		// Gather the fields under the lock. We'll do the JSON marshalling outside the lock
+		hs.countMux.Lock()
+		total := hs.count
+		avg := hs.avgUsec
+		hs.countMux.Unlock()
+
+		stat := stats{total, avg}
+		b, err := json.Marshal(stat)
+
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to marshal stats. Err: %v", err), http.StatusInternalServerError)
+		}
+
+		w.Write(b)
+
+	// Only GET requests allowed
 	default:
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 	}
@@ -140,6 +184,6 @@ func (hs *HashServer) Run() {
 	go hs.hashWorker()
 	http.HandleFunc("/hash", hs.hashPostHandler)
 	http.HandleFunc("/hash/", hs.hashGetHandler)
-	// TODO use port
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/stats", hs.statsHandler)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", hs.port), nil))
 }
